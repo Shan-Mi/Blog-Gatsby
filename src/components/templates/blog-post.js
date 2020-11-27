bdcb3a9f30d8d72e80d2502c01e93bba229046df
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-27 23:04:16 +0100

    Styled gobackbtn

--- src/components/templates/blog-post.js
+++ src/components/templates/blog-post.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Layout from '../layout';
 import { node } from 'prop-types';
+import styled from 'styled-components';
+
+const GoBackBtn = styled.button`
+  cursor: pointer;
+  background-color: #4c96d7;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: 0.25s ease-in;
+  opacity: 0.7;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
 
 export default ({ data }) => {
   const post = data.markdownRemark;
@@ -12,7 +28,7 @@ export default ({ data }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
       <Link to="/">
-        <button>Go back</button>
+        <GoBackBtn>Go back</GoBackBtn>
       </Link>
     </Layout>
   );
