9b45c28245aa3cc69e4284e59701e991fbe55cae
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-11-29 22:44:19 +0100

    Fixed image display

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,33 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/markdown-pages/black-friday.md
+++ src/markdown-pages/black-friday.md
@@ -2,12 +2,16 @@
 title: 'Black Friday'
 date: '2020-11-29'
 description: 'Inspecting everything'
+# featuredImage: ../images/black-friday.png
+
 ---
 The plan was to buy some running tights, then began to inspect website.
+
 ![inspect xxl](../images/black-friday.png)
 
 Oh man, Stadium's console is a mess...
+
 ![Stadium Console](../images/black-friday-2.png)
 
-### XXL:
-Once you go to '/cart' this check page, there is no way you can go to item detial page. What are they thinking about??
+#### About XXL:
+Once you go to **'/cart'** this check-out page, there is no way you can go to item detial page. What are they thinking about?? And they are not the only nor the last one.
